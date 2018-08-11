fd6647f7e8d5ebb19838d1e1a07324e26a26bff3
Piotr Grobelny <piotr.grobelny.pl@gmail.com>
2018-08-11 19:27:54 +0200

    added appMode

--- src/main/java/com/pgr/booking/ViewController.java
+++ src/main/java/com/pgr/booking/ViewController.java
@@ -1,5 +1,8 @@
 package com.pgr.booking;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +12,19 @@ import java.util.Date;
 @Controller
 public class ViewController {
 
+//	@Value("${app-mode}")
+	private String appMode;
 
+	@Autowired
+	public ViewController(Environment environment) {
+		appMode = environment.getProperty("app-mode");
+	}
 
 	@RequestMapping("/")
 	public String index(Model model) {
 		model.addAttribute("datetime", new Date());
 		model.addAttribute("username", "Piotrek");
-		model.addAttribute("mode", "production");
+		model.addAttribute("mode", appMode);
 
 		return "index";
 	}
